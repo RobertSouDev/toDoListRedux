5b75d44b0314d1185f19f02956dc92a857559101
RobertSouDev <rogerhallks1@gmail.com>
2024-08-11 23:01:23 -0300

    Implementando o Redux

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,16 +1,24 @@
 import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+
 
 const TodoList = () => {
+    const {list, filter} = useSelector((state)=>  state.todos)
+
   return (
     <div>
        <button>Todas</button>
        <button>Completas</button>
        <button>Incompletas</button>
        <ul>
-        <li>
-            <span>algum texto tarefa</span>
-            <button>Remover</button>
-        </li>
+        {
+            list.map((todo)=> (
+                <li key={todo.id}>
+                <span>{todo.text}</span>
+                <button>Remover</button>
+            </li>
+            ))
+        }
        </ul>
 
     </div>
