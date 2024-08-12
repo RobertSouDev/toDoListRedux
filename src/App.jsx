866ce011ccd4fb27d239dfa38dccc01197db20df
RobertSouDev <rogerhallks1@gmail.com>
2024-08-11 22:09:26 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import AddTodo from './components/AddTodo'
+import TodoList from './components/TodoList'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>lista de Tarefas com Redux</h1>
+      <AddTodo/>
+      <TodoList/>
+    </>
+  )
+}
+
+export default App
+
+
